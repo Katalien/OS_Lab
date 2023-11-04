9edd3341dc623069c58818d04aec2ce95e491c4d
KateZin <90635745+KateZin@users.noreply.github.com>
2023-11-04 19:14:15 +0300

    fixed main

--- LAB1/main.cpp
+++ LAB1/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        std::cout << "You must enter an argument" << std::endl;
+        std::cout << "You must enter the path to config file" << std::endl;
         return EXIT_FAILURE;
     }
     std::string relPath = argv[1];
