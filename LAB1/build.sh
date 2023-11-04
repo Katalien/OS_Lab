215793e1ed649fca1d6a21a8d436e179a6a0f120
KateZin <90635745+KateZin@users.noreply.github.com>
2023-11-04 19:06:32 +0300

    fix build.sh

--- LAB1/build.sh
+++ LAB1/build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+pidPath="/var/run/daemon.pid"
+[[ -f $pidPath ]] || sudo touch "$pidPath"
+sudo chmod 666 "$pidPath"
+
 mkdir build
 cd build
 cmake ..
@@ -5,3 +10,5 @@ make
 cd ..
 cp ./build/Daemon .
 rm -r build
+
+sudo chmod +x ./Daemon
